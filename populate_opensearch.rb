ca26ee51578f66d023fefb8f1efba893f96c066a
Kevin Dew <kevindew@me.com>
2024-03-17 20:39:33 +0000

    Basic files to populate and search opensearch

---
+++ populate_opensearch.rb
@@ -0,0 +1,60 @@
+require "bundler/inline"
+
+gemfile do
+  source "https://rubygems.org"
+
+  gem "debug"
+  gem "opensearch-ruby"
+end
+
+require "json"
+require "opensearch-ruby"
+
+INDEX_NAME = "chunked_govuk_content"
+
+client = OpenSearch::Client.new(url: ENV.fetch("OPENSEARCH_URL", "http://localhost:9200"))
+client.indices.delete(index: INDEX_NAME) if client.indices.exists?(index: INDEX_NAME)
+
+client.indices.create(
+  index: INDEX_NAME,
+  body: {
+    settings: {
+      index: {
+        knn: true
+      }
+    },
+    mappings: {
+      properties: {
+        content_id: { type: "keyword" },
+        locale: { type: "keyword" },
+        base_path: { type: "keyword" },
+        content_url: { type: "keyword" },
+        title: { type: "text" },
+        heading_context: { type: "text" },
+        html_content: { type: "text" },
+        plain_content: { type: "text" },
+        openai_embeddings: {
+          type: "knn_vector",
+          dimension: 1536,
+          method: {
+            name: "hnsw",
+            space_type: "l2",
+            engine: "faiss"
+          }
+        },
+        digest: { type: "keyword" }
+      }
+    }
+  }
+)
+
+Dir["mainstream_content/chunked_json/*.json"].each do |path|
+  chunked_item = JSON.load_file(path)
+  actions = chunked_item["chunks"].flat_map do |chunk|
+    action = { index: { _id: chunk["id"] } }
+    document_data = chunked_item.slice(*%w[content_id locale base_path title])
+    chunk_data = chunk.slice(*%w[content_url heading_context html_content plain_content openai_embeddings digest])
+    [action, document_data.merge(chunk_data)]
+  end
+  client.bulk(index: INDEX_NAME, body: actions)
+end
---
+++ searching_opensearch.rb
@@ -0,0 +1,52 @@
+require "bundler/inline"
+
+gemfile do
+  source "https://rubygems.org"
+
+  gem "debug"
+  gem "dotenv"
+  gem "ruby-openai"
+  gem "opensearch-ruby"
+end
+
+require "debug"
+require "dotenv/load"
+require "json"
+require "openai"
+require "opensearch-ruby"
+
+INDEX_NAME = "chunked_govuk_content"
+OPENAI_EMBEDDING_MODEL = "text-embedding-3-small"
+
+opensearch_client = OpenSearch::Client.new(url: ENV.fetch("OPENSEARCH_URL", "http://localhost:9200"))
+openai_client = OpenAI::Client.new(access_token: ENV["OPENAI_ACCESS_TOKEN"])
+
+openai_response = openai_client.embeddings(
+  parameters: { model: OPENAI_EMBEDDING_MODEL, input: "Tell me about systolic pressure" }
+)
+openai_embeddings = openai_response.dig("data", 0, "embedding")
+
+results = opensearch_client.search(
+  index: INDEX_NAME,
+  body: {
+    size: 2,
+    query: {
+      knn: {
+        openai_embeddings: {
+          vector: openai_embeddings,
+          k: 2
+        }
+      }
+    },
+    fields: [
+      "content_url",
+      "heading_context",
+      "html_content",
+      "plain_content"
+    ],
+    # avoid the noise of open ai embeddings
+    _source: false
+  }
+)
+
+debugger
